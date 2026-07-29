ccccd1739c9179162b913e729e0d0cdf92701174
Rajyadav8412 <rajyada2323230@gmail.com>
2026-07-29 21:26:57 +0530

    Completed resume upload using DRF and JWT authentication

--- resumes/models.py
+++ resumes/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Resume(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="resumes"
+    )
+
+    resume = models.FileField(
+        upload_to="resumes/"
+    )
+
+    uploaded_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return f"{self.user.username} - Resume"
--- resumes/views.py
+++ resumes/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser, FormParser
 
-# Create your views here.
+from .serializers import ResumeSerializer
+
+
+class ResumeUploadView(APIView):
+    permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request):
+
+        print("========== DEBUG ==========")
+        print("request.data :", request.data)
+        print("request.FILES:", request.FILES)
+        print("===========================")
+
+        serializer = ResumeSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data)
+
+        print(serializer.errors)
+        return Response(serializer.errors, status=400)
