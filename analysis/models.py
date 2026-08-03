d39f0d4ac3ce1d86be339e27380ea4d93abbfc21
Rajyadav8412 <rajyada2323230@gmail.com>
2026-08-03 11:48:55 +0530

    Saved user information into Database and enhanced exception handling

--- analysis/models.py
+++ analysis/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
+from resumes.models import Resume
 
-# Create your models here.
+
+class ResumeAnalysis(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    resume = models.ForeignKey(
+        Resume,
+        on_delete=models.CASCADE
+    )
+
+    extracted_text = models.TextField()
+
+    resume_data = models.JSONField()
+
+    analysis = models.JSONField()
+
+    ai_analysis = models.JSONField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.user.username} - {self.created_at}"
--- analysis/views.py
+++ analysis/views.py
@@ -8,6 +8,7 @@ from .utils.extractor import extract_resume_information
 from .utils.analyzer import analyze_resume
 from django.conf import settings
 from .utils.ai_analyzer import analyze_resume_with_ai
+from .models import ResumeAnalysis
 
 
 
@@ -27,7 +28,29 @@ class ExtractResumeTextView(APIView):
         extracted_text = extract_text_from_pdf(resume.resume.path)
         resume_data = extract_resume_information(extracted_text)
         analysis_result = analyze_resume(resume_data)
-        ai_analysis = analyze_resume_with_ai(extracted_text)
+
+        try:
+            ai_analysis = analyze_resume_with_ai(extracted_text)
+        except Exception as e:
+            print("Gemini Error:", e)
+
+            ai_analysis = {
+                "summary": "AI analysis is currently unavailable.",
+                "strengths": [],
+                "weaknesses": [],
+                "suggestions": [],
+                "ats_score": None,
+                "error": str(e)
+            }
+
+        ResumeAnalysis.objects.create(
+            user=request.user,
+            resume=resume,
+            extracted_text=extracted_text,
+            resume_data=resume_data,
+            analysis=analysis_result,
+            ai_analysis=ai_analysis,
+        )
 
         return Response({
         **resume_data,
